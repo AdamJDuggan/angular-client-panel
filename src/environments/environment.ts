33ded9023cc061300e08624f1fd1fdf9a110866f
AdamJDuggan <adamduggan17@gmail.com>
2021-05-25 11:54:39 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCftHmdzIOY8PgI64xpr1TM9zMgobunZaU',
+    authDomain: 'angular-client-panel-1a573.firebaseapp.com',
+    projectId: 'angular-client-panel-1a573',
+    storageBucket: 'angular-client-panel-1a573.appspot.com',
+    messagingSenderId: '885369386640',
+    appId: '1:885369386640:web:cf301f25e579eebfee8a15',
+    measurementId: 'G-V5G7J00JQ1',
+  },
 };
 
 /*
